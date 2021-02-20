5cf104c29c86122f882c46fb0c78232772b285ef
Nerea García <nereagarcia.c@gmail.com>
2021-02-20 19:22:40 +0100

    Creación y testeo función analisaPalabras()

--- js/analiza-palabras.js
+++ js/analiza-palabras.js
@@ -1,1 +1,31 @@
-/* Crea tu función aquí */
+/*Prueba en una consola a parte de que la función analizaPalabras() funciona. */
+
+const prueba = ["Hola", "cable", "casa"];
+
+function analizaPalabras(arrayPalabras) {
+
+    console.log("```");
+
+    arrayPalabras.filter(palabra => {
+
+        for (i = 0; i < arrayPalabras.length; i++) {
+            console.log(`#######`);
+            console.log(
+                `Palabra ${(arrayPalabras.indexOf(palabra)) + 1}: ${palabra}`);
+            console.log(`Nº caracteres: ${palabra.length}`);
+            console.log(`Deletreo: ${palabra.split("").join().replaceAll(",", "-")}`);
+            console.log(`La palabra es ${palabra.length % 2 === 0 ?
+                "par" : "impar"} y ${palabra[0] !== palabra[0].toLowerCase() ?
+                    "sí" : "no"} empieza por mayúsucla`);
+            const noincluye = "ble";
+            console.log(`La palabra ${palabra.includes(noincluye)
+                ? 'sí' : 'no'} contiene "ble"`);
+
+        }
+    });
+
+    console.log(`#######`);
+    console.log("```");
+}
+
+console.log(analizaPalabras(prueba));
